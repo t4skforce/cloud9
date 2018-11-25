56adabd49a5f3eb2782f533d0daf8a621fd5ec49
t4skforce <t4skforce1337@gmail.com>
2018-11-26 00:41:29 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,21 @@
 if [ ! -z "$ROOT_CA" ]; then
   if [ -f "${ROOT_CA}" ]; then
     if [ ! -f '/usr/local/share/ca-certificates/ca.crt' ]; then
-			cp "${ROOT_CA}" '/usr/local/share/ca-certificates/ca.crt'
-			update-ca-certificates
+      cp "${ROOT_CA}" '/usr/local/share/ca-certificates/ca.crt'
+      update-ca-certificates
       echo "CA '${ROOT_CA}' installed successfully."
     else
       echo "CA '${ROOT_CA}' is already installed"
-		fi
+    fi
   else
-		echo "file '${ROOT_CA}' does not exist!"
+    echo "file '${ROOT_CA}' does not exist!"
   fi
 fi
+if [ "$(stat -c '%u' /cloud9)" != 1000 ]; then
+  chown -R ${UID}:${GID} /cloud9
+fi
+if [ "$(stat -c '%u' ${HOME})" != 1000 ]; then
+  chown -R ${UID}:${GID} ${HOME}
+fi
 
-su -c 'forever' cloud -- "/cloud9/server.js -w ${HOME} -p ${PORT} --listen 0.0.0.0 $@"
+sudo -u "#${UID}" forever -- /cloud9/server.js -w ${HOME} -p ${PORT} --listen 0.0.0.0 $@
